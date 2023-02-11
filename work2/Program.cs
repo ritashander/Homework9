844c4cebae04f1c2bbf4bc3e4e417cf38d798024
Rita Shander <iamshizooo@gmail.com>
2023-02-11 14:19:32 +0300

    work2 done

---
+++ work2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+
+System.Console.WriteLine(sum(1, 3));
+
+
+    int sum(int n, int m) 
+{
+    if(n == m)
+    {
+        return m;
+    }
+    return n + sum(n + 1, m);
+}
